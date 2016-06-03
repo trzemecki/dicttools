5ff217565f93cc134aa2555d1a57ad5e14f47872
leszek <leszek.trzemecki@gmail.com>
2016-06-03 17:26:25 +0200

    Remove test from setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='dicttools',
     version='1.0.0',
-    packages=['tests', 'dicttools'],
+    packages=['dicttools'],
     url='https://github.com/trzemecki/dicttools',
     license='Apache 2.0',
     author='Leszek Trzemecki',
     author_email='leszek.trzemecki@gmail.com',
     description='Additional dictionary functions for python.',
-    requires=['mock']
+    requires=['mock'],
 )
