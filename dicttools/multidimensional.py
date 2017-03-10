93e9e5a4d736d7a9b48cc1bf28a1f4700b5463b0
leszek <leszek.trzemecki@gmail.com>
2017-03-10 21:04:46 +0100

    Refactor naming

--- dicttools/multidimensional.py
+++ dicttools/multidimensional.py
@@ -16,13 +16,13 @@ class MultiDict(object):
             self._init_headers(max(self._items))
 
     def _roll_array(self, data, index=()):
-        for i, item in enumerate(data):
-            item_index = index + (i,)
-            if isinstance(item, list):
-                for each in self._roll_array(item, item_index):
+        for i, value in enumerate(data):
+            value_index = index + (i,)
+            if isinstance(value, list):
+                for each in self._roll_array(value, value_index):
                     yield each
             else:
-                yield item_index, item
+                yield value_index, value
 
     @property
     def shape(self):
@@ -39,32 +39,32 @@ class MultiDict(object):
         if len(key) < self.size:
             key += (slice(None),) * (self.size - len(key))
 
-        if not any(isinstance(k, slice) for k in key):
-            return self._items[self._map_key(key)]
+        if not any(isinstance(token, slice) for token in key):
+            return self._items[self._key_index(key)]
         else:
             return _MultiDictView(self, key)
 
-    def reduce(self, key):
-        key = self._map_key(key)
-        return MultiDict(dict(self._reduce_keys(key)), headers=list(self._reduce_headers(key)))
+    def reduce(self, index):
+        index = self._key_index(index)
+        return MultiDict(dict(self._reduce_keys(index)), list(self._reduce_headers(index)))
 
-    def _reduce_keys(self, request_key):
-        for source_key, source_value in self._items.items():
+    def _reduce_keys(self, request_index):
+        for source_index, source_value in self._items.items():
             result = []
-            for request_index, source_index in zip(request_key, source_key):
-                if isinstance(request_index, list):
-                    if source_index not in request_index:
+            for request_token, source_token in zip(request_index, source_index):
+                if isinstance(request_token, list):
+                    if source_token not in request_token:
                         break
 
-                    result.append(source_index)
-                elif source_index != request_index:
+                    result.append(source_token)
+                elif source_token != request_token:
                     break
             else:
                 yield tuple(result), source_value
 
-    def _reduce_headers(self, request_key):
-        for request_index, header in zip(request_key, self._headers):
-            if isinstance(request_index, list):
+    def _reduce_headers(self, index):
+        for token, header in zip(index, self._headers):
+            if isinstance(token, list):
                 yield header
 
     def __setitem__(self, key, value):
@@ -72,9 +72,9 @@ class MultiDict(object):
             key = (key,)
 
         self._init_headers(len(key))
-        key = self._map_key(key, insert=True)
+        index = self._key_index(key, insert=True)
 
-        self._items[key] = value
+        self._items[index] = value
 
     def _init_headers(self, shape):
         if isinstance(shape, tuple):
@@ -88,32 +88,33 @@ class MultiDict(object):
         elif len(self._headers) != count:
             raise KeyError('Wrong size %d, expected %d' % (count, len(self._headers)))
 
-    def _map_key(self, request_key, insert=False):
+    def _key_index(self, key, insert=False):
         return tuple(
-            self._map_index(request_index, headers, insert)
-            for request_index, headers in zip(request_key, self._headers)
+            self._token_index(token, axis, insert) for axis, token in enumerate(key)
         )
 
-    def item_to_index(self, item, column):
-        return self._map_index(item, self._headers[column])
+    def token_to_index(self, token, axis):
+        return self._token_index(token, axis)
 
-    def _map_index(self, item, headers, insert=False):
-        if isinstance(item, slice):
-            start = 0 if item.start is None else headers.index(item.start)
-            stop = len(headers) if item.stop is None else headers.index(item.stop)
-            step = 1 if item.step is None else item.step
+    def _token_index(self, token, axis, insert=False):
+        headers = self._headers[axis]
+
+        if isinstance(token, slice):
+            start = 0 if token.start is None else headers.index(token.start)
+            stop = len(headers) if token.stop is None else headers.index(token.stop)
+            step = 1 if token.step is None else token.step
 
             return list(six.moves.range(start, stop, step))
 
         try:
-            return headers.index(item)
+            return headers.index(token)
         except ValueError:
             if not insert:
                 raise
 
-        new_item = len(headers)
-        headers.append(item)
-        return new_item
+        new_index = len(headers)
+        headers.append(token)
+        return new_index
 
     def __repr__(self):
         return repr(self._items)
@@ -174,11 +175,11 @@ class _MultiDictView(_DictView):
         result = []
 
         iter_keys = iter(other_key)
-        for col, item in enumerate(self._key):
+        for axis, item in enumerate(self._key):
             if isinstance(item, slice):
                 try:
                     reduced_item = next(iter_keys)
-                    self._check_contains(col, item, other_key, reduced_item)
+                    self._check_contains(axis, item, other_key, reduced_item)
                     result.append(reduced_item)
                     continue
                 except StopIteration:
@@ -188,15 +189,15 @@ class _MultiDictView(_DictView):
 
         return tuple(result)
 
-    def _check_contains(self, col, item, other_key, reduced_item):
-        indices = self._source.item_to_index(item, col)
+    def _check_contains(self, axis, item, other_key, reduced_item):
+        indices = self._source.token_to_index(item, axis)
         if isinstance(reduced_item, slice):
-            reduced_indices = set(self._source.item_to_index(reduced_item, col))
+            reduced_indices = set(self._source.token_to_index(reduced_item, axis))
 
             if set(indices) | reduced_indices != reduced_indices:
                 raise KeyError(other_key)
 
-        elif self._source.item_to_index(reduced_item, col) not in indices:
+        elif self._source.token_to_index(reduced_item, axis) not in indices:
             raise KeyError(other_key)
 
 
