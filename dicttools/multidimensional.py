2dffadda5389a2b0e4a4d6b077145fc83c22ed52
leszek <leszek.trzemecki@gmail.com>
2017-03-10 19:49:19 +0100

    Add dict view

--- dicttools/multidimensional.py
+++ dicttools/multidimensional.py
@@ -26,18 +26,27 @@ class MultiDict(object):
 
     @property
     def shape(self):
-        return list(map(len, self._headers))
+        return tuple(map(len, self._headers or []))
+
+    @property
+    def size(self):
+        return len(self._headers or [])
 
     def __getitem__(self, key):
         if not isinstance(key, tuple):
             key = (key,)
 
-        key = self._map_key(key)
+        if len(key) < self.size:
+            key += (slice(None),) * (self.size - len(key))
 
-        if not any(isinstance(k, list) for k in key):
-            return self._items[key]
+        if not any(isinstance(k, slice) for k in key):
+            return self._items[self._map_key(key)]
         else:
-            return MultiDict(dict(self._reduce_keys(key)))
+            return _MultiDictView(self, key)
+
+    def reduce(self, key):
+        key = self._map_key(key)
+        return MultiDict(dict(self._reduce_keys(key)), headers=list(self._reduce_headers(key)))
 
     def _reduce_keys(self, request_key):
         for source_key, source_value in self._items.items():
@@ -52,6 +61,11 @@ class MultiDict(object):
                     break
             else:
                 yield tuple(result), source_value
+
+    def _reduce_headers(self, request_key):
+        for request_index, header in zip(request_key, self._headers):
+            if isinstance(request_index, list):
+                yield header
 
     def __setitem__(self, key, value):
         if not isinstance(key, tuple):
@@ -70,34 +84,36 @@ class MultiDict(object):
             shape = [0] * count
 
         if self._headers is None:
-            self._headers = [list(six.moves.range(size)) for size in shape]
+            self._headers = [list(six.moves.range(size + 1)) for size in shape]
         elif len(self._headers) != count:
             raise KeyError('Wrong size %d, expected %d' % (count, len(self._headers)))
 
-    def _map_key(self, key, insert=False):
-        result = []
+    def _map_key(self, request_key, insert=False):
+        return tuple(
+            self._map_index(request_index, headers, insert)
+            for request_index, headers in zip(request_key, self._headers)
+        )
 
-        for each, headers in zip(key, self._headers):
-            if isinstance(each, slice):
-                start = 0 if each.start is None else headers.index(each.start)
-                stop = len(headers) if each.stop is None else headers.index(each.stop)
-                step = 1 if each.step is None else each.step
+    def item_to_index(self, item, column):
+        return self._map_index(item, self._headers[column])
 
-                indices = list(six.moves.range(start, stop, step))
-                result.append(indices)
-            else:
-                try:
-                    index = headers.index(each)
-                except ValueError:
-                    if not insert:
-                        raise
+    def _map_index(self, item, headers, insert=False):
+        if isinstance(item, slice):
+            start = 0 if item.start is None else headers.index(item.start)
+            stop = len(headers) if item.stop is None else headers.index(item.stop)
+            step = 1 if item.step is None else item.step
 
-                    index = len(headers)
-                    headers.append(each)
+            return list(six.moves.range(start, stop, step))
 
-                result.append(index)
+        try:
+            return headers.index(item)
+        except ValueError:
+            if not insert:
+                raise
 
-        return tuple(result)
+        new_item = len(headers)
+        headers.append(item)
+        return new_item
 
     def __repr__(self):
         return repr(self._items)
@@ -115,3 +131,65 @@ class MultiDict(object):
         return not self == other
 
 
+class _MultiDictView(object):
+    def __init__(self, source, key):
+        self._source = source
+        self._key = key
+
+    def __getitem__(self, key):
+        return self._source[self._merge_key_with(key)]
+
+    def __setitem__(self, key, value):
+        self._source[self._merge_key_with(key)] = value
+
+    def reduce(self, key=None):
+        return self._source.reduce(
+            self._key if key is None else self._merge_key_with(key)
+        )
+
+    def _merge_key_with(self, other_key):
+        if not isinstance(other_key, tuple):
+            other_key = (other_key,)
+
+        result = []
+
+        iter_keys = iter(other_key)
+        for col, item in enumerate(self._key):
+            if isinstance(item, slice):
+                try:
+                    reduced_item = next(iter_keys)
+                    self._check_contains(col, item, other_key, reduced_item)
+                    result.append(reduced_item)
+                    continue
+                except StopIteration:
+                    pass
+
+            result.append(item)
+
+        return tuple(result)
+
+    def _check_contains(self, col, item, other_key, reduced_item):
+        indices = self._source.item_to_index(item, col)
+        if isinstance(reduced_item, slice):
+            reduced_indices = set(self._source.item_to_index(reduced_item, col))
+
+            if set(reduced_item) | reduced_indices != reduced_indices:
+                raise KeyError(other_key)
+
+        elif self._source.item_to_index(reduced_item, col) not in indices:
+            raise KeyError(other_key)
+
+    def __eq__(self, other):
+        if isinstance(other, (tuple, list)):
+            other = MultiDict(other)
+
+        if isinstance(other, MultiDict):
+            return self.reduce() == other
+
+        return False
+
+    def __repr__(self):
+        return repr(self.reduce())
+
+    def __ne__(self, other):
+        return not self == other
--- dicttools/tests/test_multidimensional.py
+++ dicttools/tests/test_multidimensional.py
@@ -34,6 +34,97 @@ class MultiDictTest(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
+    def test_GetItem_ByCascade_ReturnValue(self):
+        instance = self.create([
+            [12, 13],
+            [25, 34],
+        ], headers=[[1, 2], ['A', 'B']])
+
+        actual = instance[1]['B']
+
+        self.assertEqual(13, actual)
+
+    def test_GetItem_KeyNotInView_Throws(self):
+        instance = self.create([
+            [12, 13, 14],
+            [25, 34, 35],
+        ], headers=[[1, 2], ['A', 'B', 'C']])
+
+        view = instance[1, 'A':'B']
+
+        with self.assertRaises(KeyError):
+            actual = view['C']
+
+    def test_Shape_DictHasValues_ReturnTupleWithDimensions(self):
+        instance = self.create([
+            [12, 13],
+            [25, 34],
+            [56, 89],
+        ])
+
+        actual = instance.shape
+
+        self.assertEqual((3, 2), actual)
+
+    def test_Shape_DictNotInit_ReturnEmptyTuple(self):
+        instance = self.create()
+
+        actual = instance.shape
+
+        self.assertEqual((), actual)
+
+    def test_Size_DictHasValues_ReturnNumberOfDimensions(self):
+        instance = self.create([
+            [12, 13],
+            [25, 34],
+            [56, 89],
+        ])
+
+        actual = instance.size
+
+        self.assertEqual(2, actual)
+
+    def test_Size_DictNotInit_Return0(self):
+        instance = self.create()
+
+        actual = instance.size
+
+        self.assertEqual(0, actual)
+
+    def test_SetItem_ByTuple_ReplaceValueWithGiven(self):
+        instance = self.create([
+            [12, 13],
+            [25, 34],
+            [56, 89],
+        ])
+
+        instance[1, 1] = 65
+
+        expected = [
+            [12, 13],
+            [25, 65],
+            [56, 89],
+        ]
+
+        self.assertEqual(expected, instance)
+
+    def test_SetItem_InView_ReplaceValueWithGiven(self):
+        instance = self.create([
+            [12, 13],
+            [25, 34],
+            [56, 89],
+        ])
+
+        instance[1][1] = 65
+
+        expected = [
+            [12, 13],
+            [25, 65],
+            [56, 89],
+        ]
+
+        self.assertEqual(expected, instance)
+
     @staticmethod
     def create(*args, **kwargs):
         return dicttools.multidimensional.MultiDict(*args, **kwargs)
